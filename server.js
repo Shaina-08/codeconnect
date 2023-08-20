d21835a349c49b962838855cb0d936c55bf9e2e3
Shaina-08 <20bee040@nith.ac.in>
2023-08-20 11:22:29 +0530

    Updates

---
+++ server.js
@@ -0,0 +1,63 @@
+import express from 'express';
+// import path from 'path';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+const app = express();
+// import { ACTIONS } from './src/Actions';
+// import { JOIN, JOINED } from './src/Actions';
+const server = createServer(app);
+const io = new Server(server);
+
+//  app.use (express.static('build'));
+// app.use((req, res, next) =>{
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// })
+const userSocketMap = {};
+
+function getAllConnectedClients(roomId){
+   return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
+    return{
+        socketId,
+        username: userSocketMap[socketId],
+    };
+   });
+}
+
+io.on('connection',(socket) =>{
+    console.log('socket connected ', socket.id);
+    socket.on('join', ({roomId, username})=>{
+        userSocketMap[socket.id] = username;
+        socket.join(roomId);
+        const clients = getAllConnectedClients(roomId);
+        // console.log(clients);
+        clients.forEach(({socketId}) =>{
+              io.to(socketId).emit('joined',{
+                clients,
+                username,
+                socketId: socket.id,
+              });
+        });
+    });
+
+    // socket.on('sync-code', ({socketId, value})=>{
+    //     io.to(socketId).emit('code-change', {code})
+    // })
+
+    socket.on('disconnecting', () => {
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomId) =>{
+             socket.in(roomId).emit('disconnected', {
+                socketId: socket.id,
+                username: userSocketMap[socket.id],
+             });
+        });
+    delete userSocketMap[socket.id];
+    socket.leave();
+
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
